f86886621bb7e5c95795be81cd3bc1ee79ef37ff
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-09-10 00:15:50 +0330

    feat: embed MapViewController in navigation controller
    - Embedded MapViewController in a navigation controller with the navigation bar hidden.
    - Added new SearchViewController and connected it to its class.
    - Added a button in MapViewController with constraints.
    - Configured the button to present SearchViewController modally on tap.

---
+++ NeshanAPI/ViewControllers/SearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SearchViewController.swift
+//  NeshanAPI
+//
+//  Created by Mahdi on 6/20/1403 AP.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
