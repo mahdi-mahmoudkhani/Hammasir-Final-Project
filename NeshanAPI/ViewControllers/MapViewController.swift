0dd25e19626cccabc52f54f5c8072888ace27ddb
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-09-09 15:47:31 +0330

    feat: add MapView to main storyboard and set up constraints
    - Added a MKMapView to the storyboard.
    - Created an IBOutlet for the MapView.
    - Applied Auto Layout constraints to ensure the MapView fills the entire screen, including the area below the safe area.

--- NeshanAPI/ViewControllers/MapViewController.swift
+++ NeshanAPI/ViewControllers/MapViewController.swift
@@ -6,9 +6,12 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
