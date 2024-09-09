e69d876a2c3dcd73ec01a4be8d29406497827d76
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-09-10 00:36:12 +0330

    feat: add search field to SearchViewController with constraints
    - Added a search field to SearchViewController with appropriate constraints.
    - Created an IBOutlet for the search field in SearchViewController for future use.

--- NeshanAPI/ViewControllers/SearchViewController.swift
+++ NeshanAPI/ViewControllers/SearchViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
