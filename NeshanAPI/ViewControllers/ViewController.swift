f7227af5ad2e7ac694667cad915741575f503f93
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-09-09 15:35:11 +0330

    init: organize project into MVC architecture
    - Added new folders to align with MVC structure:
      - Models
      - ViewControllers
      - Storyboards
      - Services
      - Assets
      - SupportingFiles
    - Moved existing files to their corresponding folders to reflect the new structure.

---
+++ NeshanAPI/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NeshanAPI
+//
+//  Created by Mahdi on 6/19/1403 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
