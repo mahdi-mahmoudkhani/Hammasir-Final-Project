84ac2ddb763425e00d8ba9da658643dc76fcdde3
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-09-09 23:47:42 +0330

    feat: integrate Combine to monitor location authorization
    - Added @Published property to LocationManager to observe authorization status.
    - Implemented checkLocationAuthorization to handle various location authorization states.
    - Updated locationManagerDidChangeAuthorization to call checkLocationAuthorization.
    - In the view controller, subscribed to authorization changes using Combine.
    - Dynamically updated the map view’s user tracking button and mode based on authorization status.

--- NeshanAPI/Services/LocationManager.swift
+++ NeshanAPI/Services/LocationManager.swift
@@ -6,16 +6,46 @@
 //
 
 import CoreLocation
+import Combine
 
 class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     
-    private let locationManager =  CLLocationManager()
+    @Published var authorization: CLAuthorizationStatus?
+    private var locationManager: CLLocationManager?
     
     override init() {
         
         super.init()
-        self.locationManager.delegate = self
-        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager = CLLocationManager()
+        self.locationManager!.delegate = self
     }
     
+    private func checkLocationAuthorization() {
+        
+        guard let locationManager = self.locationManager else { return }
+        
+        switch locationManager.authorizationStatus {
+            
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+            
+        case .restricted:
+            print("Show an allert to let them know")
+            
+        case .denied:
+            print("Show an allert to let them turn it on")
+            
+        case .authorizedAlways, .authorizedWhenInUse:
+            self.authorization = .authorizedWhenInUse
+            
+        @unknown default:
+            break
+        }
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        
+        self.checkLocationAuthorization()
+    }
+
 }
--- NeshanAPI/ViewControllers/MapViewController.swift
+++ NeshanAPI/ViewControllers/MapViewController.swift
@@ -7,24 +7,34 @@
 
 import UIKit
 import MapKit
+import Combine
 
 class MapViewController: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
     private var locationManager: LocationManager?
+    private var cancellables = Set<AnyCancellable>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
         
-        self.mapView.userTrackingMode = .follow
         self.locationManager = LocationManager()
+        
+        self.locationManager?.$authorization
+            .sink { authorization in
+                if authorization == .denied || authorization == .restricted || authorization == .none{
+                    self.mapView.showsUserTrackingButton = false
+                    
+                } else {
+                    self.mapView.showsUserTrackingButton = true
+                    self.mapView.userTrackingMode = .follow
+                }
+            }
+            .store(in: &self.cancellables)
+
     }
     
 
