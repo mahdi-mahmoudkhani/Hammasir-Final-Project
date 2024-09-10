e33dbf5c6b0c331a28a608281eb75c68b39203f1
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-09-10 20:48:54 +0330

    feat(APIConfig.swift): add APIConfig struct with static apiKey and baseURL for Neshan API

---
+++ NeshanAPI/Services/APIConfig.swift
@@ -0,0 +1,14 @@
+//
+//  APIConfig.swift
+//  NeshanAPI
+//
+//  Created by Mahdi on 6/20/1403 AP.
+//
+
+import Foundation
+
+struct APIConfig {
+    
+    static let apiKey = "service.ed27586460264e0180659f8ce893937d"
+    static let baseURL = "https://api.neshan.org/v1/search?"
+}
